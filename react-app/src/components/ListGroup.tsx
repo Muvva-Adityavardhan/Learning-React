665088215c4718a4da85eab82631709c9473f2f4
Muvva Adityavardhan <muvva.adityavardhan@gmail.com>
2025-06-10 23:34:59 +0530

    Rendering lists

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,17 +1,18 @@
-
-
 function ListGroup() {
+	const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+	// items.map(item => {
+	//     return <li className="list-group-item">{item}</li>;
+	// });
 	return (
 		<>
-            <h1>ListGroup</h1>
-            <ul className="list-group">
-                <li className="list-group-item">An item</li>
-                <li className="list-group-item">A second item</li>
-                <li className="list-group-item">A third item</li>
-                <li className="list-group-item">A fourth item</li>
-                <li className="list-group-item">And a fifth one</li>
-            </ul>
-        </>
+			<h1>ListGroup</h1>
+			<ul className="list-group">
+				{items.map((item) => (
+					<li key={item}>{item}</li>
+				))}
+			</ul>
+		</>
 	);
 }
 
