9942c84277438ae1ae8bc09284c0c6f7295cc5b9
Muvva Adityavardhan <muvva.adityavardhan@gmail.com>
2025-06-10 23:49:13 +0530

    Conditional rendering

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -2,21 +2,10 @@ function ListGroup() {
 	let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 	//items = []; // Uncomment this line to test the "No items found" case
 
-	const message = items.length === 0 ? <p>No items found</p> : null;
-
-	const getMessage = () => {
-		return items.length === 0 ? <p>No items found</p> : null;
-	};
-
 	return (
 		<>
 			<h1>ListGroup</h1>
-			{getMessage()}
-			{message}
-			{items.length === 0 ? <p>No items found</p> : null}
-
-            {/* The above three lines are equivalent and demonstrate different ways to conditionally render content */}
-
+            {items.length === 0 && <p>No items found</p>}
 			<ul className="list-group">
 				{items.map((item) => (
 					<li key={item}>{item}</li>
