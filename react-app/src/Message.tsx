acce45e265443597cb6fd046aa25c92b5d8dbc58
Muvva Adityavardhan <muvva.adityavardhan@gmail.com>
2025-06-10 21:52:18 +0530

    Adding conditional statements

--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -1,7 +1,10 @@
 //PascalCasing
 function Message() {
-	const name = "Aditya"; //Variable declaration
-	return <h1>Hello {name}</h1>; //JSX syntax --> JavaScript XML
+	const name = "Aditya";
+	if (name == "Aditya")
+		//Variable declaration
+		return <h1>Hello {name}</h1>; //JSX syntax --> JavaScript XML
+	return <h1>Welcome</h1>;
 }
 
 export default Message; //Exporting the component
